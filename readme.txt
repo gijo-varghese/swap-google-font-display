034ccd5300b17dc00efb395c42c661c273b1353e
Gijo Varghese <gijovarghese141@gmail.com>
2021-10-18 17:16:24 +0530

    initial commit

---
+++ readme.txt
@@ -0,0 +1,70 @@
+=== Swap Google Fonts Display ===
+
+Contributors: gijo, flyingpress
+Tags: fonts, google fonts, web font, display swap
+Requires at least: 3.0.1
+Tested up to: 5.8
+Stable tag: 1.0.7
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+Ensure text remains visible during webfont load
+
+## Features
+- Inject display=swap to Google Fonts
+
+## Support
+- [Official Support Forum](https://wordpress.org/support/plugin/swap-google-font-display/)
+- [Facebook Community](https://www.facebook.com/groups/wpspeedmatters)
+
+## More Optimization Plugins
+- [FlyingPress](https://flying-press.com) (premium)
+- [Flying Pages](https://wordpress.org/plugins/flying-pages/)
+- [Flying Images](https://wordpress.org/plugins/nazy-load/)
+- [Flying Scripts](https://wordpress.org/plugins/flying-scripts/)
+- [Flying Analytics](https://wordpress.org/plugins/flying-analytics/)
+- [Flying Fonts](https://wordpress.org/plugins/flying-fonts/)
+
+== Installation ==
+
+Just install the plugin and active it. No further configuration is needed.
+
+== Changelog ==
+
+= 1.0.7 =
+
+- Updated plugin links
+
+= 1.0.6 =
+
+- Bug fixes while using W3 Total Cache
+- Bug fixes while using custom wp-includes folder
+
+= 1.0.5 =
+
+- Support for W3 Total Cache
+
+= 1.0.4 =
+
+- Automatically add font-display:swap to all css files when using LiteSpeed cache
+
+= 1.0.3 =
+
+- Checks entire HTML for Google Fonts (previously <head> only)
+
+= 1.0.2 =
+
+- Support for Web Font Loader
+
+= 1.0.1 =
+
+- Removed unwanted CSS and JS files
+- Removed unwanted boilerplate php files
+- Prevent multiple display swaps (if already added by theme/plugins)
+- Added donation link
+
+= 1.0 =
+
+- First release!
---
+++ swap-google-font-display.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Plugin Name:       Swap Google Fonts Display
+ * Plugin URI:        https://wordpress.org/plugins/swap-google-font-display/
+ * Description:       Ensure text remains visible during webfont load
+ * Version:           1.0.7
+ * Author:            FlyingPress
+ * Author URI:        https://flying-press.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       swap-google-font-display
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+define( 'GOOGLE_FONT_DISPLAY_SWAPPER_VERSION', '1.0.6' );
+
+// Inject dispaly=swap to Google Fonts
+function google_fonts_ds_inject_display_swap($html) {
+
+    // Remove existing display swaps
+    $html = str_replace("&#038;display=swap", "", $html);
+	
+	// Add font-display=swap as a querty parameter to Google fonts
+    $html = str_replace("googleapis.com/css?family", "googleapis.com/css?display=swap&family", $html);
+
+    // Fix for Web Font Loader
+    $html = preg_replace("/(WebFontConfig\['google'\])(.+[\w])(.+};)/", '$1$2&display=swap$3', $html);
+
+    return $html;
+  
+}
+
+// Capture HTML
+function google_fonts_ds_capture_html() {
+    ob_start("google_fonts_ds_inject_display_swap");
+}
+add_action('init', 'google_fonts_ds_capture_html', 1);
+
+if (!defined('FLYING_PRESS_VERSION')) {
+  add_filter('plugin_action_links_' . plugin_basename(__FILE__), function ($links) {
+    $plugin_shortcuts[] =
+      '<a href="https://flying-press.com?ref=swap_google_fonts_display" target="_blank" style="color:#3db634;">Get FlyingPress</a>';
+    return array_merge($links, $plugin_shortcuts);
+  });
+}
+
+// Add font-display:swap using LiteSpeed cache
+function google_fonts_ds_litespee_cache($content, $file_type, $urls) {
+    if ($file_type === 'css')
+        $content =  str_replace('@font-face{','@font-face{font-display:swap;', $content);
+    return $content;
+}
+add_filter('litespeed_optm_cssjs', 'google_fonts_ds_litespee_cache', 10, 3);;
